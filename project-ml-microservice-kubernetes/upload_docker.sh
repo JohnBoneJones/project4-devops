ae42e56cc4d96ca5652a59368a8ac8ddc463ac20
JonJones <minhtuanjr00@gmail.com>
2023-06-03 12:21:10 +0700

    add files

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=namtn6/project-4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u bigpolarbear
+docker tag project-4 $dockerpath:latest
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
